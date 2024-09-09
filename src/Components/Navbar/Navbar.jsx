9f9d23d1a474a40978fab33d011ba83e44f2f0a4
JuwelShaikh <juwelshaikh04@gmail.com>
2024-09-09 17:28:34 +0600

    All links added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,3 +1,5 @@
+import { FaCaretDown } from 'react-icons/fa';
+
 const NavLinks = [
     {
         id: 1,
@@ -16,25 +18,71 @@ const NavLinks = [
     }
 ];
 
+const DropdownLinks = [
+    {
+        id: 1,
+        name: 'Vegetables',
+        link: '/#'
+    },
+    {
+        id: 2,
+        name: 'Fruits',
+        link: '/#'
+    },
+    {
+        id: 1,
+        name: 'Grains',
+        link: '/#'
+    },
+]
+
 const Navbar = () => {
     return (
-        <div className="bg-white shadow-sm">
+        <div className="bg-white shadow-md">
             <div className="container flex justify-between py-4 sm:py-3">
                 {/* logo section */}
-                <div className="font-bold">
+                <div className="font-bold text-3xl">
                     logo
                 </div>
                 {/* navlinks section */}
                 <div>
                     <ul className="flex items-center gap-10">
                         {
-                           NavLinks.map(({id, name, link})=>(
-                            <li key={id}>
-                                <a href={link}>{name}</a>
-                            </li>
-                           )
-                        )
+                            NavLinks.map(({ id, name, link }) => (
+                                <li key={id}>
+                                    <a href={link} className="inline-block hover:text-rose-600 text-xl font-semibold">{name}</a>
+                                </li>
+                            )
+                            )
                         }
+                        {/* dropdown */}
+                        <li className='cursor-pointer group '>
+                            <a href="#" className="inline-block hover:text-rose-600 text-xl font-semibold">
+                                <div className='flex items-center gap-[2px] py-2'>
+                                    Catagories
+                                    <span>
+                                        <FaCaretDown className=' group-hover:rotate-180 duration-300' />
+                                    </span>
+                                </div>
+
+                            </a>
+                            {/* dropdown item */}
+                            <div className='absolute z-[9999] hidden group-hover:block w-[200px] bg-white text-black shadow-md'>
+                                
+                                <ul>
+                                    {
+                                        DropdownLinks.map(({ id, name, link }) => (
+                                            <li key={id}>
+                                                <a href={link} className='inline-block hover:bg-rose-200 rounded-md p-2 text-xl font-semibold'>
+                                                    
+                                                    {name}
+                                                </a>
+                                            </li>
+                                        ))
+                                    }
+                                </ul>
+                            </div>
+                        </li>
                     </ul>
                 </div>
             </div>
