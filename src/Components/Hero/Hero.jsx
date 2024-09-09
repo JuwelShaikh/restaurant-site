94baa6edaa57189465049a2e21e736ef7bcceb2f
JuwelShaikh <juwelshaikh04@gmail.com>
2024-09-09 18:09:17 +0600

    Primary btn compo added on process

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
-
+import HeroImg from '../../assets/hero.png'
+import PrimaryButton from '../Shared/PrimaryButton'
 
 const Hero = () => {
     return (
@@ -7,11 +8,18 @@ const Hero = () => {
                 <div className="container">
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px]">
                         {/* textArea */}
-                        <div>
-                            <h1>Fresh & healty meal plan delivery in Dhaka</h1>
-                            <p>Delicious meals delivered to your door at $100.00 per week</p>
+                        <div className='space-y-7 text-gray-500 order-2 sm:order-1'>
+                            <h1 className='text-5xl'>Fresh & healthy meal plan <span className='text-blue-400 font-cursive text-7xl'>Delivery</span><br /> in Dhaka</h1>
+                            <p className='lg:pr-64'>Delicious meals delivered to your door at $100.00 per week</p>
+
+                           <div>
+                            <PrimaryButton/>
+                           </div>
                         </div>
                         {/* imageArea */}
+                        <div className='order-1 sm:order-2'>
+                            <img src={HeroImg} alt="img1" />
+                        </div>
 
                     </div>
 
