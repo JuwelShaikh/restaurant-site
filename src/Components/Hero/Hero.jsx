d200ea03dfebc35b4a2bb6d50c1dee0a27186ab1
JuwelShaikh <juwelshaikh04@gmail.com>
2024-09-09 17:47:07 +0600

    Assets added

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,25 @@
+
+
+const Hero = () => {
+    return (
+        <>
+            <div className="relative z-[-1]">
+                <div className="container">
+                    <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px]">
+                        {/* textArea */}
+                        <div>
+                            <h1>Fresh & healty meal plan delivery in Dhaka</h1>
+                            <p>Delicious meals delivered to your door at $100.00 per week</p>
+                        </div>
+                        {/* imageArea */}
+
+                    </div>
+
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default Hero
