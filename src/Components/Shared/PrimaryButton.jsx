80aac170cd208601f5514cbae1c38f1a66911ecb
JuwelShaikh <juwelshaikh04@gmail.com>
2024-09-09 18:17:58 +0600

    Primary btn on work

--- src/Components/Shared/PrimaryButton.jsx
+++ src/Components/Shared/PrimaryButton.jsx
@@ -1,9 +1,17 @@
+import { FaArrowRight } from "react-icons/fa"
 
 
 const PrimaryButton = () => {
-  return (
-    <div>PrimaryButton</div>
-  )
+    return (
+        <>
+            <div className="flex items-center group cursor-pointer">
+                <button className="bg-rose-600 h-[40px] text-white px-3 py-2">
+                    Choose your meal plan
+                </button>
+                <FaArrowRight className="inline-block group-hover:!translate-x-2 duration-200"/>
+            </div>
+        </>
+    )
 }
 
 export default PrimaryButton
