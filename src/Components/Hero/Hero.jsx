6d2ef21e6bd6d949e6e4aa0bb9c198c727022ee2
JuwelShaikh <juwelshaikh04@gmail.com>
2024-09-10 12:55:08 +0600

    primary btn added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,21 +4,21 @@ import PrimaryButton from '../Shared/PrimaryButton'
 const Hero = () => {
     return (
         <>
-            <div className="relative z-[-1]">
-                <div className="container">
+            <div className="">
+                <div className="container p-16 sm:py-0">
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 place-items-center min-h-[600px]">
                         {/* textArea */}
                         <div className='space-y-7 text-gray-500 order-2 sm:order-1'>
                             <h1 className='text-5xl'>Fresh & healthy meal plan <span className='text-blue-400 font-cursive text-7xl'>Delivery</span><br /> in Dhaka</h1>
                             <p className='lg:pr-64'>Delicious meals delivered to your door at $100.00 per week</p>
-
-                           <div>
+                            {/* btnArea */}
+                           <div className='cursor-pointer'>
                             <PrimaryButton/>
                            </div>
                         </div>
                         {/* imageArea */}
-                        <div className='order-1 sm:order-2'>
-                            <img src={HeroImg} alt="img1" />
+                        <div className='relative z-30 order-1 sm:order-2'>
+                            <img src={HeroImg} alt="img1" className='w-full sm:translate-y-16 sm:scale-125' />
                         </div>
 
                     </div>
--- src/Components/Shared/PrimaryButton.jsx
+++ src/Components/Shared/PrimaryButton.jsx
@@ -4,11 +4,11 @@ import { FaArrowRight } from "react-icons/fa"
 const PrimaryButton = () => {
     return (
         <>
-            <div className="flex items-center group cursor-pointer">
+            <div className="flex items-center group">
                 <button className="bg-rose-600 h-[40px] text-white px-3 py-2">
                     Choose your meal plan
                 </button>
-                <FaArrowRight className="inline-block group-hover:!translate-x-2 duration-200"/>
+                <FaArrowRight className="inline-block group-hover:!translate-x-2 duration-200 bg-rose-800  text-white h-[40px] w-[40px]" />
             </div>
         </>
     )
