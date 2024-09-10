09cf8e61cc4765166d06cb469d8c4b6c05290a1a
JuwelShaikh <juwelshaikh04@gmail.com>
2024-09-10 17:45:09 +0600

    Footer on process

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./Components/About/About"
 import Banner from "./Components/Banner/Banner"
+import Footer from "./Components/Footer/Footer"
 import Hero from "./Components/Hero/Hero"
 import Navbar from "./Components/Navbar/Navbar"
 import WhyChoose from "./Components/WhyChoose/WhyChoose"
@@ -13,6 +14,7 @@ const App = () => {
       <WhyChoose/>
       <Banner/>
       <About/>
+      <Footer/>
     </div>
   )
 }
--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,26 @@
+import { IoLocationSharp } from "react-icons/io5"
 
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <>
+            <div className="text-white mt-20">
+                <div className="container bg-gradient-to-b from-rose-500 to-rose-700 rounded-t-3xl">
+                    {/* contact section */}
+                    <h1>
+                        Contact us
+                    </h1>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-14 border-b-2 border-white pb-6">
+                        <div>
+                            <div>
+                                <IoLocationSharp className="text-5xl" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Footer
