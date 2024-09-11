b20084b51a5e305f084d81cb07ee5e3e5889f43a
JuwelShaikh <juwelshaikh04@gmail.com>
2024-09-11 23:17:32 +0600

    Log in popup

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import About from "./Components/About/About"
 import Banner from "./Components/Banner/Banner"
 import Footer from "./Components/Footer/Footer"
 import Hero from "./Components/Hero/Hero"
+import LoginPage from "./Components/Login page/LoginPage"
 import Navbar from "./Components/Navbar/Navbar"
 import WhyChoose from "./Components/WhyChoose/WhyChoose"
 
@@ -14,6 +15,7 @@ const App = () => {
       <WhyChoose/>
       <Banner/>
       <About/>
+      <LoginPage/>
       <Footer/>
     </div>
   )
